c91e005796d4a1a38cf4f0c9bb70b6da9842dd5a
calvin <c4lvin18@gmail.com>
2026-06-26 21:10:06 +0800

    1. Add top emission by nation chart. 2. Exclude non country values from owid dataset.

---
+++ heuristic1/chart/top_emission_nation.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import plotly.express as px
+import plotly.colors as pc
+
+
+def chart(data: pd.DataFrame, year: int, top: int):
+    snapshot = data[data["year"] == year]
+
+    sorted = snapshot.sort_values("co2_including_luc", ascending=False)
+    top_countries = sorted.head(top)
+    other_total = sorted.iloc[top:]["co2_including_luc"].sum()
+
+    chart_data = pd.concat([
+        top_countries,
+        pd.DataFrame([{"country": "Other", "co2_including_luc": other_total}])
+    ])
+
+    fig = px.pie(
+        chart_data,
+        names="country",
+        values="co2_including_luc",
+    )
+
+    max = sorted["co2_including_luc"].max()
+    colors = list(pc.sample_colorscale("YlOrRd", top_countries["co2_including_luc"] / max))
+    colors.append("lightgray")
+
+    fig.update_traces(
+        textinfo="label+percent",
+        marker=dict(colors=colors),
+    )
+
+    return fig
--- heuristic1/page.py
+++ heuristic1/page.py
@@ -1,20 +1,38 @@
 import streamlit as st
 
 from heuristic1.loaders import country_geojson, owid
-from heuristic1.chart import global_nation_co2_emission
+from heuristic1.chart import global_nation_co2_emission, top_emission_nation
 
 st.set_page_config(layout="wide")
+
+
+def overview_section():
+    map, top = st.columns([3, 1])
+
+    with map:
+        st.plotly_chart(
+            global_nation_co2_emission.chart(
+                owid.load(), country_geojson.load(), selected_year),
+            use_container_width=True,
+        )
+
+    with top:
+        top_number = 5
+        st.subheader(f"Top {top_number} Countries by CO₂ Emissions")
+
+        st.plotly_chart(
+            top_emission_nation.chart(owid.load(), selected_year, top_number)
+        )
+
 
 with st.sidebar:
     st.header("Filters")
 
     selected_year = st.selectbox(
         "Year",
-        options=list(range(2024, 2009, -1)), # TODO: Change to actual year range base on data
+        # TODO: Change to actual year range base on data
+        options=list(range(2024, 2009, -1)),
         index=0
     )
 
-st.plotly_chart(
-    global_nation_co2_emission.chart(owid.load(), country_geojson.load(), selected_year),
-    use_container_width=True,
-)
+overview_section()
